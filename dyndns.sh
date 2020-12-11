07a755cff3dd6900d36d2b35d97850abd1838c0a
Raspbian France <raspbianfrance@gmail.com>
2020-12-11 14:38:54 +0100

    Update dyndns.sh
    fix for IPv4 only

--- dyndns.sh
+++ dyndns.sh
@@ -11,7 +11,7 @@ DOMAIN_NAME=''
 ####DO NOT TOUCH#####
 #####################
 
-PUBLIC_IP=$(host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')
+PUBLIC_IP=$(host -4 myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}')
 
 #Call OVH for update
 curl --silent --user "$DYNHOST_ID:$DYNHOST_PASSWORD" "https://www.ovh.com/nic/update?system=dyndns&hostname=$DOMAIN_NAME&myip=$PUBLIC_IP"
